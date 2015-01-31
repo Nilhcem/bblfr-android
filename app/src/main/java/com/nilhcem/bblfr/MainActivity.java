a386888c7237e805a314ba60c33f31cd1295e2b3
Gautier Nilhcem <nilhcem@gmail.com>
2015-01-31 12:32:51 +0100

    Add poc code to get baggers

--- app/src/main/java/com/nilhcem/bblfr/MainActivity.java
+++ app/src/main/java/com/nilhcem/bblfr/MainActivity.java
@@ -1,7 +1,16 @@
 package com.nilhcem.bblfr;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nilhcem.bblfr.model.Baggers;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +18,36 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AsyncTask<Void, Void, Baggers>() {
+
+            @Override
+            protected Baggers doInBackground(Void... params) {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(BuildConfig.WS_ENDPOINT + BuildConfig.WS_BAGGERS_URL)
+                        .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    String body = response.body().string();
+                    // Json starts with "var data = {" and ends with ";"
+                    body = body.replaceFirst("[^{]*", "");
+                    body = body.substring(0, body.length() - 1);
+
+                    // Serialisation
+                    ObjectMapper mapper = new ObjectMapper();
+                    return mapper.readValue(body, Baggers.class);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Baggers baggers) {
+                super.onPostExecute(baggers);
+
+            }
+        }.execute();
     }
 }
