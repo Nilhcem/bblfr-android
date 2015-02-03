e3aad863cb0115dbb527d89846eeca6f01864bea
Gautier Nilhcem <nilhcem@gmail.com>
2015-02-03 21:23:25 +0100

    Unsubscribe when SplashscreenActivity stops

--- app/src/main/java/com/nilhcem/bblfr/ui/splashscreen/SplashscreenActivity.java
+++ app/src/main/java/com/nilhcem/bblfr/ui/splashscreen/SplashscreenActivity.java
@@ -13,16 +13,18 @@ import com.nilhcem.bblfr.ui.SecondActivity;
 import javax.inject.Inject;
 
 import butterknife.InjectView;
+import rx.Subscription;
 import rx.android.app.AppObservable;
-import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
-public class SplashscreenActivity extends BaseActivity implements Action1<Boolean> {
+public class SplashscreenActivity extends BaseActivity {
 
     @Inject ImportBaggersService mService;
 
     @InjectView(R.id.splash_subtitle) TextView mSubtitle;
+
+    private Subscription mSubscription;
 
     public SplashscreenActivity() {
         super(R.layout.splashscreen_activity);
@@ -32,15 +34,18 @@ public class SplashscreenActivity extends BaseActivity implements Action1<Boolea
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mSubtitle.setText(Html.fromHtml(getString(R.string.splash_subtitle)));
-
-        AppObservable.bindActivity(this, mService.importBaggers())
+        mSubscription = AppObservable.bindActivity(this, mService.importBaggers())
                 .subscribeOn(Schedulers.io())
-                .subscribe(this);
+                .subscribe(success -> {
+                    Timber.d("Import successful: " + success);
+                    startActivity(new Intent(this, SecondActivity.class));
+
+                });
     }
 
     @Override
-    public void call(Boolean success) {
-        Timber.d("Import successful: " + success);
-        startActivity(new Intent(this, SecondActivity.class));
+    protected void onStop() {
+        mSubscription.unsubscribe();
+        super.onStop();
     }
 }
