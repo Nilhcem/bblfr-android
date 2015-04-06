9b2210bb7f91b586eb7c2c2d87e561f740de6466
Gautier Nilhcem <nilhcem@gmail.com>
2015-04-06 13:43:25 +0200

    Create toolbar

--- app/src/main/java/com/nilhcem/bblfr/ui/BaseActivity.java
+++ app/src/main/java/com/nilhcem/bblfr/ui/BaseActivity.java
@@ -2,15 +2,21 @@ package com.nilhcem.bblfr.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.nilhcem.bblfr.BBLApplication;
+import com.nilhcem.bblfr.R;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.Optional;
 
 public abstract class BaseActivity extends ActionBarActivity {
 
     protected BBLApplication mApplication;
     private final int mLayoutResId;
+
+    @Optional @InjectView(R.id.toolbar) protected Toolbar mToolbar;
 
     protected BaseActivity(int layoutResId) {
         mLayoutResId = layoutResId;
@@ -26,6 +32,10 @@ public abstract class BaseActivity extends ActionBarActivity {
         if (mLayoutResId != 0) {
             setContentView(mLayoutResId);
             ButterKnife.inject(this);
+
+            if (mToolbar != null) {
+                setSupportActionBar(mToolbar);
+            }
         }
     }
 }
