b5e78f9a8b5e299137c678cd0582b52dd5e07db0
Gautier Nilhcem <nilhcem@gmail.com>
2015-04-06 21:59:27 +0200

    Add method to get cities

--- app/src/main/java/com/nilhcem/bblfr/model/baggers/dao/CitiesDao.java
+++ app/src/main/java/com/nilhcem/bblfr/model/baggers/dao/CitiesDao.java
@@ -1,12 +1,22 @@
 package com.nilhcem.bblfr.model.baggers.dao;
 
+import com.nilhcem.bblfr.model.baggers.City;
+
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import ollie.query.Select;
 
 @Singleton
 public class CitiesDao {
 
     @Inject
     public CitiesDao() {
+    }
+
+    public List<City> getCities() {
+        return Select.from(City.class).fetch();
     }
 }
