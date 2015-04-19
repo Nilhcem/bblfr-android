51913985dca2d2fb2d963e032e4c6b786fdf8b2c
Gautier Nilhcem <nilhcem@gmail.com>
2015-04-19 16:54:12 +0200

    Review sonarqube configuration

--- scripts/run-tests-unit.sh
+++ scripts/run-tests-unit.sh
@@ -3,6 +3,6 @@
 # avoid virtual desktop jumps at mac terminal on test run
 export JAVA_TOOL_OPTIONS='-Djava.awt.headless=true'
 
-./gradlew testRealDebug
+./gradlew :app:testRealDebug
 
 echo "test reports: $(pwd)/app/build/reports/tests/realDebug/index.html"
