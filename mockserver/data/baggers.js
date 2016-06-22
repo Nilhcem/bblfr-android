4afed2c0a62421fb8aad63ffa941b150699a6db4
Gautier Nilhcem <nilhcem@gmail.com>
2016-06-22 17:58:10 +0200

    Update mock server baggers data to the new data model

--- mockserver/data/baggers.js
+++ mockserver/data/baggers.js
@@ -1,95 +1,101 @@
 var data = {
-    "baggers": [
-       {
-            "name": "George Waterfall",
-            "bio": "Agile comme l'eau tombant d'une cascade. Je rigole lorsqu'on me vante l'agilité d'une mêlée de rugby ou d'un panneau canne-banne",
-            "picture": "img/george_waterfall.jpg",
-            "websites": [
-                {
-                    "title": "Linkedin",
-                    "href": "https://www.linkedin.com/in/georgewaterfall/"
-                }
+    "speakers": [{
+        "name": "George Waterfall",
+        "bio": "Agile comme l'eau tombant d'une cascade. Je rigole lorsqu'on me vante l'agilité d'une mêlée de rugby ou d'un panneau canne-banne",
+        "picture": "img/george_waterfall.jpg",
+        "websites": [{
+            "name": "LinkedIn",
+            "url": "https://www.linkedin.com/in/georgewaterfall/"
+        }],
+        "location": "Paris",
+        "sessions": [{
+            "title": "Le cycle en W",
+            "abstract": "Retour aux fondamentaux : Le cycle en V.<br />L'idée de cette méthode révolutionnaire est de cumuler deux V pour en faire un <b>W</b>. On recommence tout après le premier cycle pour améliorer notre produit et montrer qu'on a de la thune.",
+            "tags": [
+                "ScrumCestNul",
+                "KanbanCestPasMieux"
             ],
-            "twitter": "georgewaterfall",
-            "contact": "contact@georgewaterf.all",
-            "mail": "contact@georgewaterf.all",
-            "location": "Paris",
-            "sessions": [
-                {
-                    "title": "Le cycle en W",
-                    "summary": "Retour aux fondamentaux : Le cycle en V.<br />L'idée de cette méthode révolutionnaire est de cumuler deux V pour en faire un <b>W</b>. On recommence tout après le premier cycle pour améliorer notre produit et montrer qu'on a de la thune."
-                },
-                {
-                    "title": "Les avantages indéniables du management par la terreur",
-                    "summary": "Comment, comme moi, vous pourrez augmenter votre turnover, et révolutionner votre entreprise en apportant du sang neuf simplement en haussant un peu la voix et en changeant légèrement votre vocabulaire."
-                }
-            ],
+            "lang": [
+                "fr"
+            ]
+        }, {
+            "title": "Les avantages indéniables du management par la terreur",
+            "abstract": "Comment, comme moi, vous pourrez augmenter votre turnover, et révolutionner votre entreprise en apportant du sang neuf simplement en haussant un peu la voix et en changeant légèrement votre vocabulaire.",
             "tags": [
                 "AgileIsDead",
-                "ScrumCestNul",
-                "KanbanCestPasMieux",
                 "XP_MDR"
             ],
-            "cities": [
-                "Paris",
-                "Lyon"
+            "lang": [
+                "fr"
             ]
-        },
-        {
-            "name": "Jack Auffoltrayde",
-            "bio": "Développeur passionné. Je code plus vite que mon ombre.",
-            "picture": "img/jack_trade.jpg",
-            "websites": [
-                {
-                    "title": "AbstractProxyFactoryDecorator",
-                    "href": "http://AbstractProxyFactoryDecorator.com/"
-                }
-            ],
-            "twitter": "jack_alltrades",
-            "contact": "jack [at] just_jack.co",
-            "mail": "jack@just_jack.co",
-            "location": "Paris",
-            "sessions": [
-                {
-                    "title": "Montrez votre supériorité",
-                    "summary": "Si l'on arrive à lire votre code facilement, c'est qu'il est trop simple. Honorez votre salaire. Cumulez design patterns et méthodes récursives. Committez votre code obfusqué pour gagner en perf. Montrez aux autres que le dev, le vrai, c'est vous."
-                },
-                {
-                    "title": "Étendre le NoMock Movement",
-                    "summary": "L'inutilité des mocks n'est plus à démontrer. Maintenant il faut aller plus loin. On vit dans le concret, on mange sa propre nourriture pour chien en production. Les serveurs de test ne servent à rien. Il faut trouver ses bugs en prod pour être sûr qu'ils existent réellement."
-                }
-            ],
+        }],
+        "cities": [
+            "Paris",
+            "Lyon"
+        ],
+        "contacts": {
+            "twitter": "georgewaterfall",
+            "mail": "contact@georgewaterf.all"
+        }
+    }, {
+        "name": "Jack Auffoltrayde",
+        "bio": "Développeur passionné. Je code plus vite que mon ombre.",
+        "picture": "img/jack_trade.jpg",
+        "websites": [{
+            "name": "Web",
+            "url": "http://AbstractProxyFactoryDecorator.com/"
+        }],
+        "location": "Paris",
+        "sessions": [{
+            "title": "Montrez votre supériorité",
+            "abstract": "Si l'on arrive à lire votre code facilement, c'est qu'il est trop simple. Honorez votre salaire. Cumulez design patterns et méthodes récursives. Committez votre code obfusqué pour gagner en perf. Montrez aux autres que le dev, le vrai, c'est vous.",
             "tags": [
-                "NoMock",
                 "Crapmanship",
                 "KeepItReal",
-                "TDD_Faggot",
                 "XP_MDR"
             ],
-            "cities": [
-                "Paris",
-                "Nantes"
+            "lang": [
+                "fr"
             ]
-      }
-    ],
-    "cities": [
-        {
+        }, {
+            "title": "Étendre le NoMock Movement",
+            "abstract": "L'inutilité des mocks n'est plus à démontrer. Maintenant il faut aller plus loin. On vit dans le concret, on mange sa propre nourriture pour chien en production. Les serveurs de test ne servent à rien. Il faut trouver ses bugs en prod pour être sûr qu'ils existent réellement.",
+            "tags": [
+                "NoMock",
+                "KeepItReal",
+                "TDD_Faggot"
+            ],
+            "lang": [
+                "fr"
+            ]
+        }],
+        "cities": [
+            "Paris",
+            "Nantes"
+        ],
+        "contacts": {
+            "twitter": "jack_alltrades",
+            "mail": "jack@just_jack.co"
+        }
+    }],
+    "cities": {
+        "Lyon": {
             "name": "Lyon",
             "ville_img": "img/BBL_Lyon.jpg",
             "lat": 45.764043,
             "lng": 4.835659
         },
-        {
+        "Nantes": {
             "name": "Nantes",
             "ville_img": "img/BBL_Nantes.jpg",
             "lat": 47.219102,
             "lng": -1.551132
         },
-        {
+        "Paris": {
             "name": "Paris",
             "ville_img": "img/BBL_Paris.jpg",
             "lat": 48.856614,
             "lng": 2.352222
         }
-    ]
+    }
 };
