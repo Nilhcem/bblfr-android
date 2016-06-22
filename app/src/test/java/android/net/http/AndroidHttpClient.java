54e97b3903a2b3c6a15f48d5d798739f023b4d41
Gautier Nilhcem <nilhcem@gmail.com>
2016-06-22 12:48:05 +0200

    Create stub AndroidHttpClient due to a Robolectric issue targetting api 23+
    https://github.com/robolectric/robolectric/issues/1862

---
+++ app/src/test/java/android/net/http/AndroidHttpClient.java
@@ -0,0 +1,6 @@
+package android.net.http;
+
+// Robolectric has issues when creating Shadows targetting API 23+
+// https://github.com/robolectric/robolectric/issues/1862
+public class AndroidHttpClient {
+}
