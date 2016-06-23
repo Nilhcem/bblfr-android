29d50d8baca0c448a0bd6eea9dc867c7e614a089
Gautier Nilhcem <nilhcem@gmail.com>
2016-06-23 11:08:29 +0200

    Only select matching sessions (and not matching speakers) when selecting a tag

--- app/src/main/java/com/nilhcem/bblfr/model/baggers/dao/BaggersDao.java
+++ app/src/main/java/com/nilhcem/bblfr/model/baggers/dao/BaggersDao.java
@@ -68,12 +68,12 @@ public class BaggersDao {
         List<Bagger> baggers = QueryUtils.rawQuery(Bagger.class, sql.toString(), args.toArray(new String[args.size()]));
 
         for (Bagger bagger : baggers) {
-            fillBaggerData(bagger);
+            fillBaggerData(bagger, tagsIds);
         }
         return baggers;
     }
 
-    private void fillBaggerData(@NonNull Bagger bagger) {
+    private void fillBaggerData(@NonNull Bagger bagger, @NonNull List<String> tagsIds) {
         // contacts
         bagger.contacts = Select.from(Contact.class).where("contacts.bagger_id=?", bagger.id).fetchSingle();
 
@@ -85,16 +85,15 @@ public class BaggersDao {
         }
 
         // sessions
-        bagger.sessions = Select.from(Session.class).where("bagger_id=?", bagger.id).fetch();
-
-        // tags
-        for (Session session : bagger.sessions) {
+        List<Session> sessions = Select.from(Session.class).where("bagger_id=?", bagger.id).fetch();
+        for (Session session : sessions) {
             List<Tag> tags = Select.from(Tag.class).innerJoin(SessionTag.class).on("tags._id=sessions_tags.tag_id").where("sessions_tags.session_id=?", session.id).fetch();
             session.tags = new ArrayList<>();
             for (Tag tag : tags) {
                 session.tags.add(tag.name);
             }
         }
+        bagger.sessions = filterSessionsMatchingTags(sessions, tagsIds);
 
         // websites
         bagger.websites = Select.from(Website.class).where("bagger_id=?", bagger.id).fetch();
@@ -106,5 +105,48 @@ public class BaggersDao {
     public List<Tag> getSessionsTags(@NonNull Long cityId) {
         String sql = "SELECT DISTINCT tags.* FROM sessions_tags INNER JOIN tags ON sessions_tags.tag_id=tags._id INNER JOIN sessions ON sessions_tags.session_id=sessions._id INNER JOIN baggers_cities ON sessions.bagger_id=baggers_cities.bagger_id WHERE baggers_cities.city_id=? GROUP BY sessions_tags.tag_id ORDER BY COUNT(sessions_tags.tag_id) DESC";
         return QueryUtils.rawQuery(Tag.class, sql, new String[]{Long.toString(cityId)});
+    }
+
+    private List<Session> filterSessionsMatchingTags(@NonNull List<Session> sessions, @NonNull List<String> tagsIds) {
+        if (tagsIds.isEmpty()) {
+            return sessions;
+        }
+
+        List<Session> filtered = new ArrayList<>();
+        List<String> tags = getTagsNamesPerIds(tagsIds);
+        for (Session session : sessions) {
+            for (String tag : tags) {
+                if (session.tags.contains(tag)) {
+                    filtered.add(session);
+                    break;
+                }
+            }
+        }
+        return filtered;
+    }
+
+    private List<String> getTagsNamesPerIds(@NonNull List<String> tagsIds) {
+        boolean addSeparator = false;
+        List<String> args = new ArrayList<>();
+
+        StringBuilder whereStr = new StringBuilder("_id IN (");
+        for (String tagId : tagsIds) {
+            if (addSeparator) {
+                whereStr.append(",");
+            } else {
+                addSeparator = true;
+            }
+
+            whereStr.append("?");
+            args.add(tagId);
+        }
+        whereStr.append(")");
+
+        List<Tag> tags = Select.from(Tag.class).where(whereStr.toString(), args.toArray(new String[args.size()])).fetch();
+        List<String> tagsNames = new ArrayList<>(tags.size());
+        for (Tag tag : tags) {
+            tagsNames.add(tag.name);
+        }
+        return tagsNames;
     }
 }
